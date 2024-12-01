5e014eb138755d8957ac58279d78918b503b3728
Kush Jain <kush.jain@innovaccer.com>
2024-12-01 23:44:33 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,15 +1,22 @@
 import socket  # noqa: F401
 
 
+def handle_client(client):
+    # Handle client communication here.
+    client.recv(1024).decode("utf-8")
+    client.sendall(b"+PONG\r\n")
+
+
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     # print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    comm, _ = server_socket.accept() # wait for client
-    comm.sendall(b"+PONG\r\n")
+    client, _ = server_socket.accept()  # wait for client
+
+    while True:
+        handle_client(client)  # handle client connection
 
 
 if __name__ == "__main__":
     main()
-    
